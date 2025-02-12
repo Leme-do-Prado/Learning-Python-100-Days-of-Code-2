38d5b77b785bcf26a37cbd56a41b195654f6bd0d
Leandro <102317613+Leme-do-Prado@users.noreply.github.com>
2025-02-12 19:25:41 -0300

    Add files via upload

--- task.py
+++ task.py
@@ -1,12 +1,13 @@
 
 print("Hello! Let's calculate your suggested tip and bill splits. \n"
       + "Im going to ask you some questions now.")
-tipPercentage = float(input("1. What's your desired tip percentage? \n"))/100
-totalAmount = float(input("2. What's the total bill amount? \n"))
+tipPercentage = float(input("1. What's your desired tip percentage?\n"))/100
+totalAmount = float(input("2. What's the total bill amount? \n$"))
 peopleAmount = int(input("3. How many people you're in? \n"))
 
 
 splitBill = (totalAmount * (1 + tipPercentage)) / peopleAmount
+splitBill = round(splitBill, 2)
 
 print("Your suggested tip is " + str(totalAmount * tipPercentage))
 print("The suggested splits are of " + str(splitBill) + " pounds each.")
